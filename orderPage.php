d25642b351118c2726f62238e6ea6c37196f390a
guy.sartorelli@gmail.com <guy.sartorelli@gmail.com>
2018-11-12 18:33:39 +1300

    Added some javascript verification and check before increment

--- orderPage.php
+++ orderPage.php
@@ -7,58 +7,62 @@
 	$port    = 9090;
 	$socket = socket_create(AF_INET, SOCK_STREAM, 0);
 	$result = socket_connect($socket, $host, $port);
-	$message1 = "RGSTR,WEB\r\n";
-	socket_write($socket, $message1, strlen($message1)) or die("Could not send data to server\n");
-	//<!--request categories & ingredients, call getIngredients and parse into array(5), for each item check quantity greater than 0 and display if so-->
-
-
-	$message2 = "REQ_CAT\r\n";
-	socket_write($socket, $message2, strlen($message2)) or die("Could not send data to server\n");
-	$categoriesRaw = socket_read ($socket, 1024, PHP_NORMAL_READ) or die("Could not read server response\n");
-	$categoriesRaw = str_replace("SEND_CAT,", "", $categoriesRaw);
-	$categories = explode(",", $categoriesRaw);
-
-
-	$message3 = "REQ_INGR\r\n";
-	socket_write($socket, $message3, strlen($message3)) or die("Could not send data to server\n");
-	$ingredientsRaw = socket_read ($socket, 1024, PHP_NORMAL_READ) or die("Could not read server response\n");
-	$ingredientsRaw = str_replace("SEND_INGR,", "", $ingredientsRaw);
-	$ingredients1D = explode(",", "$ingredientsRaw");
-	
-	//parse ingredients string to multidimensional associative array
-	$ingredients = array();
-	$category = "NONE";
-	$ingredient = "NONE";
-	for ($i = 0; $i < sizeof($ingredients1D); $i++){
-		switch ($i % 5) {
-		    case 0:
-    	        $category = $ingredients1D[$i];
-    	        $ingredients[$category] = array();
-    	        break;
-        	case 1:
-        	    $ingredient = $ingredients1D[$i];
-        	    $ingredients[$category][$ingredient] = array();
-        	    $ingredients[$category][$ingredient]["name"] = $ingredient;
-            	break;
-    		case 2:
-    		    $ingredients[$category][$ingredient]["price"] = doubleval($ingredients1D[$i]);
-    			break;
-    		case 3:
-    		    $ingredients[$category][$ingredient]["quantity"] = intval($ingredients1D[$i]);
-    			break;
-		}
+	if ($result === true) {
+    	$message1 = "RGSTR,WEB\r\n";
+    	socket_write($socket, $message1, strlen($message1)) or die("Could not send data to server\n");
+    	//<!--request categories & ingredients, call getIngredients and parse into array(5), for each item check quantity greater than 0 and display if so-->
+    
+    
+    	$message2 = "REQ_CAT\r\n";
+    	socket_write($socket, $message2, strlen($message2)) or die("Could not send data to server\n");
+    	$categoriesRaw = socket_read ($socket, 1024, PHP_NORMAL_READ) or die("Could not read server response\n");
+    	$categoriesRaw = str_replace("SEND_CAT,", "", $categoriesRaw);
+    	$categories = explode(",", $categoriesRaw);
+    
+    
+    	$message3 = "REQ_INGR\r\n";
+    	socket_write($socket, $message3, strlen($message3)) or die("Could not send data to server\n");
+    	$ingredientsRaw = socket_read ($socket, 1024, PHP_NORMAL_READ) or die("Could not read server response\n");
+    	$ingredientsRaw = str_replace("SEND_INGR,", "", $ingredientsRaw);
+    	$ingredients1D = explode(",", "$ingredientsRaw");
+    	
+    	//parse ingredients string to multidimensional associative array
+    	$ingredients = array();
+    	$category = "NONE";
+    	$ingredient = "NONE";
+    	for ($i = 0; $i < sizeof($ingredients1D); $i++){
+    		switch ($i % 5) {
+    		    case 0:
+        	        $category = $ingredients1D[$i];
+        	        $ingredients[$category] = array();
+        	        break;
+            	case 1:
+            	    $ingredient = str_replace(" ", "_", $ingredients1D[$i]);
+            	    $ingredients[$category][$ingredient] = array();
+            	    $ingredients[$category][$ingredient]["name"] = $ingredient;
+                	break;
+        		case 2:
+        		    $ingredients[$category][$ingredient]["price"] = doubleval($ingredients1D[$i]);
+        			break;
+        		case 3:
+        		    $ingredients[$category][$ingredient]["quantity"] = intval($ingredients1D[$i]);
+        			break;
+    		}
+    	}
+    	
+    	//send ingredients to JS
+    	echo "<script>";
+    	   echo"var ingredients = "; echo json_encode($ingredients, JSON_HEX_TAG);
+    	echo "</script>";
+    
+    
+    	//<!--DRGSTR and close the socket. -->
+    	$message3 = "DERGSTR\r\n";
+    	socket_write($socket, $message3, strlen($message3));
+    	socket_close($socket);
+	} else {
+	    echo "Error: Could not connect to ingredients server";
 	}
-	
-	//send ingredients to JS
-	echo "<script>";
-	   echo"var ingredients = "; echo json_encode($ingredients, JSON_HEX_TAG);
-	echo "</script>";
-
-
-	//<!--DRGSTR and close the socket. -->
-	$message3 = "DERGSTR\r\n";
-	socket_write($socket, $message3, strlen($message3));
-	socket_close($socket);
 
 ?>
 
@@ -83,20 +87,18 @@
 				    unset($_SESSION['order_status']);
 				}
 				?>
-				<form name="orderform" action="successfulOrderPage.php"
-					method="post">
+				<form name="orderform" onsubmit="event.preventDefault(); validate();">
 					<div>
 						<div><label for="bun" id="mainlabel">Choose Bun: </label>
-    						<select id="bunType"
-    							name="bun_type">
+    						<select id="bunType" name="bun_type" onchange="onBunChange();">
     							<option value=""></option>
     							<?php
-    							foreach ($ingredients["bread"] as $ingredient) {
+    							foreach ($ingredients["sauce"] as $ingredient) {
     							    echo("<option value = \"$ingredient[name]\">$ingredient[name]</option>");
     							}
     							?>
     						</select>
-						<input type="text" class="cost" name="bunCost" value="$0.00" disabled></input>
+						<input type="text" id="bunCost" class="cost" name="bunCost" value="$0.00" disabled></input>
 						</div>
 						<div>
 						<label for="ingredients" id="mainlabel">Choose Ingredients:</label>
@@ -104,7 +106,7 @@
 						<?php 
 // 						NOT patty, bread, sauce
 						foreach ($categories as $category){
-						    if ($category != "patty" && $category != "bread" && $category != "sauce"){
+						    if ($category != "patty" && $category != "bread" && $category != "bg"){
 						        if (array_key_exists("$category", $ingredients)){
     						        foreach($ingredients["$category"] as $ingredient){
     						            if ($ingredient["quantity"] > 0){
@@ -153,7 +155,7 @@
 						</div>
 						<div>
 							<label for="name" id="mainlabel">Customer Name:</label> 
-							<input type="text" id="name" name="order_name">
+							<input type="text" id="name" name="order_name" onkeydown="onNameChange();" onpaste="onNameChange();" oninput="onNameChange();">
 						</div>
 						<div>
 						<label for="cost" id="totalCost">Total Cost:</label>
--- script.js
+++ script.js
@@ -1,36 +1,113 @@
 //use ingredients passed from php to alter price etc
 console.log(ingredients);
 
-function incrementValue(e) {
-  e.preventDefault();
-  var fieldName = $(e.target).data('field');
-  var parent = $(e.target).closest('div');
-  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+function onBunChange() {
+	let bun = document.getElementById('bunType').value;
+	let priceField = document.getElementById('bunCost');
+	// change priceField according to price in ingredients
+}
 
-  if (!isNaN(currentVal)) {
-    parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
-  } else {
-    parent.find('input[name=' + fieldName + ']').val(0);
-  }
+// validates characters in namefield as data entered or pasted
+function onNameChange() {
+	let nameField = document.getElementById('name');
+	let name = nameField.value;
+	name = name.replace(/NONUM/g, "");// removes all instances of the string
+										// "NONUM"
+	name = name.replace(/\W/g, ""); // removes all non alphanumeric characters
+	nameField.value = name;
+}
+
+// validates and then posts the form on button click
+async function validate() {
+	let isValid = true;
+	let bun = document.getElementById('bunType').value;
+	let name = document.getElementById('name').value;
+	if (bun === "") {
+		isValid = false;
+		// alert user that it's invalid 'cause no bun
+	}
+	if (name === "") {
+		isValid = false;
+		// alert user that they must have a name
+	}
+	let sauce = document.getElementById('sauceType').value;
+	let patty = document.getElementById('pattyType').value;
+	
+	//orderNumber,customerName,ingredientCategory,ingredientName,num,ingredientCategory,ingredientName,num etc
+	let order = "NONUM,"+name+",bread,"+bun+",1,sauce,"+sauce+",1,patty,"+patty+",1";	
+
+	// ingredients.category.ingredient
+	let notMiscCategories = ["sauce", "patty", "bread"];
+	for (let category of Object.keys(ingredients)) {
+		if (!notMiscCategories.includes(category)){
+			for (let ingredient of Object.keys(ingredients[category])){
+				let quantity = document.getElementById(ingredients[category][ingredient].name+'_qty').value;
+				if (quantity > 0){
+					order += ","+category+","+ingredients[category][ingredient].name+","+quantity;
+				}
+			}
+		}
+	}
+
+	let response = await fetch("submitOrder.php", {
+		 				 method: 'POST',
+						 headers: { 'Content-type': 'application/x-www-form-urlencoded', },
+						 body: 'order=' + encodeURIComponent(order)
+						 });
+	let result = await response.text();
+	
+	if (result.startsWith("0,")){
+		//failed. Display a message and refresh the ingredients
+	} else if (result.startsWith("1,")){
+		//success. Forward to successfulOrderPage with the order number.
+	} 
+}
+
+function incrementValue(e) {
+	e.preventDefault();
+	let fieldName = $(e.target).data('field');
+	let parent = $(e.target).closest('div');
+	let currentVal = parseInt(parent.find('input[name=' + fieldName + ']')
+			.val(), 10);
+	
+	if (!isNaN(currentVal)) {
+		let ingredient = parent.find('input[name=' + fieldName + ']').attr('id').replace("_qty", "");
+		if (findIngredient(ingredient).quantity > currentVal){
+			parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+		}
+	} else {
+		parent.find('input[name=' + fieldName + ']').val(0);
+	}
 }
 
 function decrementValue(e) {
-  e.preventDefault();
-  var fieldName = $(e.target).data('field');
-  var parent = $(e.target).closest('div');
-  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+	e.preventDefault();
+	let fieldName = $(e.target).data('field');
+	let parent = $(e.target).closest('div');
+	let currentVal = parseInt(parent.find('input[name=' + fieldName + ']')
+			.val(), 10);
 
-  if (!isNaN(currentVal) && currentVal > 0) {
-    parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
-  } else {
-    parent.find('input[name=' + fieldName + ']').val(0);
-  }
+	if (!isNaN(currentVal) && currentVal > 0) {
+		parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+	} else {
+		parent.find('input[name=' + fieldName + ']').val(0);
+	}
+}
+
+function findIngredient(toFind){
+	for (let category of Object.keys(ingredients)) {
+		for (let ingredient of Object.keys(ingredients[category])){
+			if (ingredients[category][ingredient].name === toFind) {
+				return ingredients[category][ingredient];
+			}
+		}
+	}
 }
 
 $('.input-group').on('click', '.button-plus', function(e) {
-  incrementValue(e);
+	incrementValue(e);
 });
 
 $('.input-group').on('click', '.button-minus', function(e) {
-  decrementValue(e);
+	decrementValue(e);
 });
